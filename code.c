11c3c5f54d5c894394c4a7f803093e6d8cdac155
abdurrahmaans <46393995+abdurrahmaans@users.noreply.github.com>
2020-09-28 18:59:35 +0530

    Add files via upload

---
+++ code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char buffer[50];
+int main(void) {
+FILE *f = fopen("abd.txt", "w");
+if (f == NULL) {
+    printf("error in fopen\n");
+    return -1;
+}
+while (fgets(buffer, 10, stdin) != NULL){
+    if (!strcmp(buffer, "end\n")) 
+            break;
+    fputs(buffer, f);
+}
+fclose(f);
+}
